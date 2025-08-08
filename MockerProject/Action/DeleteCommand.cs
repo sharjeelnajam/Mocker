d5d5c61befe58fa24c3a68288208a86bcfd27b99
Sharjeel Najam <najamsharjeel@gmail.com>
2025-08-09 01:01:40 +0500

    Fix delete Functionality and allignment

--- MockerProject/Action/DeleteCommand.cs
+++ MockerProject/Action/DeleteCommand.cs
@@ -20,8 +20,27 @@ namespace MockerProject.Action
 
         public void Execute()
         {
-          //  _element.contro
-            _canvas.Children.Remove(_element);
+            // Safe removal that works with any type of parent container
+            if (_element.Parent is Panel panel)
+            {
+                panel.Children.Remove(_element);
+            }
+            else if (_element.Parent is Decorator decorator)
+            {
+                decorator.Child = null;
+            }
+            else if (_element.Parent is ContentControl contentControl)
+            {
+                contentControl.Content = null;
+            }
+            else if (_element.Parent != null)
+            {
+                // Fallback for other types of parents
+                var parentProp = _element.Parent.GetType().GetProperty("Children");
+                parentProp?.GetValue(_element.Parent)?.GetType()
+                    .GetMethod("Remove")?
+                    .Invoke(parentProp.GetValue(_element.Parent), new object[] { _element });
+            }
         }
 
         public void UnExecute()
